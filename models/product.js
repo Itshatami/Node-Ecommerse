d529648649cd044485474308c6cfe2403f8951aa
Alireza Hatami <itshatami@gmail.com>
2023-11-29 13:54:21 +0330

    organize product model structure

--- models/product.js
+++ models/product.js
@@ -1,32 +1,32 @@
 const fs = require('fs')
 const path = require('path')
+
+const p = path.join(__dirname , '..' , 'data' , 'products.json');
+
+const getFileFromProducts = cd =>{
+   fs.readFile(p , (err , data)=>{
+      if(err){
+         cd([]);
+      }else{
+         cd(JSON.parse(data));
+      }
+   })
+}
 exports.Product = class Product {
    constructor(title){
       this.title = title
    }
 
    save(){
-      const p = path.join(__dirname , '..' , 'data' , 'products.json');
-      fs.readFile(p , (err , data)=>{
-         let products = []
-         if(!err){
-            products = JSON.parse(data);
-         }
-         products.push(this)
-         fs.writeFile(p , JSON.stringify(products) , (err)=>{
-            console.log(err);
-         })
-      })
+         getFileFromProducts(products =>{
+            products.push(this)
+            fs.writeFile(p , JSON.stringify(products) , (err)=>{
+               console.log(err);
+            })
+         });
    }
 
    static fetchAll(cd){
-      const p = path.join(__dirname , '..' , 'data' , 'products.json');
-      fs.readFile(p , (err , data)=>{
-         if(err){
-            cd([]);
-         }else{
-            cd(JSON.parse(data));
-         }
-      })
+      getFileFromProducts(cd)
    }
 }
